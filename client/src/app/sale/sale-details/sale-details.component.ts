66436bf83187839facbb3de5c4a5aafe242a5f8d
SuperOfficeDevNet <tony@superoffice.com>
2019-02-21 10:09:30 +0100

    Fixed issue with SaleType. Updated vunerabilities.

--- client/src/app/sale/sale-details/sale-details.component.ts
+++ client/src/app/sale/sale-details/sale-details.component.ts
@@ -1,19 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-import { SaleService } from '../../services/sale.service';
-import { ContactService } from '../../services/contact.service';
-import { PersonService } from '../../services/person.service';
-import { ListService } from '../../services/list.service';
-import { UdefService } from '../../services/udef.service';
-import { ActivatedRoute, Router, ParamMap } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { SaleService } from "../../services/sale.service";
+import { ContactService } from "../../services/contact.service";
+import { PersonService } from "../../services/person.service";
+import { ListService } from "../../services/list.service";
+import { UdefService } from "../../services/udef.service";
+import { ActivatedRoute, Router, ParamMap } from "@angular/router";
+import { FormsModule } from "@angular/forms";
 
 @Component({
-  selector: 'app-sale-details',
-  templateUrl: './sale-details.component.html',
-  styleUrls: ['./sale-details.component.css']
+  selector: "app-sale-details",
+  templateUrl: "./sale-details.component.html",
+  styleUrls: ["./sale-details.component.css"]
 })
 export class SaleDetailsComponent implements OnInit {
-
   public selectedSaleId;
   public selectedSale;
   public errorMessage;
@@ -24,53 +23,62 @@ export class SaleDetailsComponent implements OnInit {
   public people;
   public contacts;
 
-  constructor(private saleSvc: SaleService, private listSvc: ListService, private contactSvc: ContactService,
-    private personSvc: PersonService, private udefSvc: UdefService, private router: Router, private route: ActivatedRoute) { }
+  constructor(
+    private saleSvc: SaleService,
+    private listSvc: ListService,
+    private contactSvc: ContactService,
+    private personSvc: PersonService,
+    private udefSvc: UdefService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
-
-    this.listSvc.getSaleTypeList().subscribe(saleTypes => { this.saleTypeList = saleTypes; });
+    this.listSvc.getSaleTypeList().subscribe(saleTypes => {
+      this.saleTypeList = saleTypes;
+    });
     this.getUserDefinedFieldTypes();
 
-    const saleId = this.route.paramMap.subscribe(
-      (params: ParamMap) => {
-        this.selectedSaleId = params.get('id');
-        if (this.selectedSaleId > 0) {
-          this.saleSvc.getSale(this.selectedSaleId)
-          .subscribe(sale => {
-             this.selectedSale = sale;
-             this.setContactAndPersonFields();
-          });
-        } else {
-          this.saleSvc.getSale(0)
-          .subscribe(sale => {
-            this.selectedSale = sale;
-            this.setContactAndPersonFields();
-          });
-        }
+    const saleId = this.route.paramMap.subscribe((params: ParamMap) => {
+      const id = params.get("id");
+
+      this.selectedSaleId = id;
+      if (this.selectedSaleId > 0) {
+        this.saleSvc.getSale(this.selectedSaleId).subscribe(sale => {
+          this.selectedSale = sale;
+          this.setContactAndPersonFields();
+        });
+      } else {
+        this.saleSvc.getSale(0).subscribe(sale => {
+          this.selectedSale = sale;
+          this.setContactAndPersonFields();
+        });
+      }
     });
 
-    this.contactSvc.getContacts()
-    .subscribe( contactData => {
-      this.contacts = contactData.value;
-    }, error => {
-      this.errorMessage = error;
-    });
+    this.contactSvc.getContacts().subscribe(
+      contactData => {
+        this.contacts = contactData.value;
+      },
+      error => {
+        this.errorMessage = error;
+      }
+    );
   }
 
   setContactAndPersonFields() {
-    if(!this.selectedSale.Contact){
+    if (!this.selectedSale.Contact) {
       this.selectedSale.Contact = { ContactId: 0 };
     } else {
       this.people = this.getAllPeople(this.selectedSale.Contact.ContactId);
     }
-    if(!this.selectedSale.Person) {
-      this.selectedSale.Person =  { PersonId:  0 };
+    if (!this.selectedSale.Person) {
+      this.selectedSale.Person = { PersonId: 0 };
     }
   }
 
   getUserDefinedFieldTypes() {
-    this.udefSvc.getAllSaleUdefFields().subscribe ( udefs => {
+    this.udefSvc.getAllSaleUdefFields().subscribe(udefs => {
       this.udefDefinitions = udefs;
     });
   }
@@ -85,30 +93,36 @@ export class SaleDetailsComponent implements OnInit {
   }
 
   saveSale() {
-    this.saleSvc.saveSale(this.selectedSale)
-    .subscribe(sale => {
-      this.selectedSale = sale;
-    }, error => {
-      this.errorMessage = error;
-    }, () => {
-      // saved...
-      this.goBack();
-    });
+    this.saleSvc.saveSale(this.selectedSale).subscribe(
+      sale => {
+        this.selectedSale = sale;
+      },
+      error => {
+        this.errorMessage = error;
+      },
+      () => {
+        // saved...
+        this.goBack();
+      }
+    );
   }
 
   deleteSale(saleId) {
-    this.saleSvc.deleteSale(saleId)
-    .subscribe(sale => {
-      const ok = sale;
-    }, error => {
-      this.errorMessage = error;
-    }, () => {
-      this.goBack();
-    });
+    this.saleSvc.deleteSale(saleId).subscribe(
+      sale => {
+        const ok = sale;
+      },
+      error => {
+        this.errorMessage = error;
+      },
+      () => {
+        this.goBack();
+      }
+    );
   }
 
   goBack() {
-    this.router.navigateByUrl('/sale');
+    this.router.navigateByUrl("/sale");
   }
 
   onSaleTypeSelected(eventData) {
@@ -116,33 +130,36 @@ export class SaleDetailsComponent implements OnInit {
   }
 
   getAllPeople(contactId) {
-
     if (+contactId === 0) {
       this.people = null;
       return;
     }
 
-    this.personSvc.getAllPerson(contactId)
-    .subscribe( personData => {
-      this.people = personData;
-    }, error => {
-      this.errorMessage = error;
-    });
+    this.personSvc.getAllPerson(contactId).subscribe(
+      personData => {
+        this.people = personData;
+      },
+      error => {
+        this.errorMessage = error;
+      }
+    );
   }
 
   contactChanged(contactId) {
-      this.selectedSale.Contact.ContactId = contactId;
+    this.selectedSale.Contact.ContactId = contactId;
 
-      this.getAllPeople(contactId);
+    this.getAllPeople(contactId);
 
-      this.selectedSale.Person = { PersonId: 0 };
+    this.selectedSale.Person = { PersonId: 0 };
   }
 
   personChanged(personId) {
-      this.selectedSale.Person.PersonId = personId;
+    this.selectedSale.Person.PersonId = personId;
   }
 
   getSaveTitle(): string {
-    return this.selectedSale.SaleId > 0 ? `PUT api/v1/Sale/${this.selectedSale.SaleId} (sale json in body)` : `POST api/v1/Sale (sale json in body)`;
+    return this.selectedSale.SaleId > 0
+      ? `PUT api/v1/Sale/${this.selectedSale.SaleId} (sale json in body)`
+      : `POST api/v1/Sale (sale json in body)`;
   }
 }
--- client/src/app/services/sale.service.ts
+++ client/src/app/services/sale.service.ts
@@ -1,37 +1,30 @@
+import { catchError } from "rxjs/operators";
+import { Injectable, Injector } from "@angular/core";
+import { Observable } from "rxjs";
 
-import {catchError} from 'rxjs/operators';
-import { Injectable, Injector } from '@angular/core';
-import { Observable } from 'rxjs';
-
-
-import { SoBaseService } from '../services/sobase.service';
+import { SoBaseService } from "../services/sobase.service";
 
 // import { HttpResponse } from 'selenium-webdriver/http';
 
 @Injectable()
 export class SaleService extends SoBaseService {
-
-  private queryAllSales = {
-    'ProviderName': 'FindSale',
-    'Columns': 'saleId, heading, description',
-    'Restrictions': 'saleId > 0',
-    'Entities': 'sale',
-    'Page': 0,
-    'PageSize': 20
-  };
-
   constructor(private injector: Injector) {
-    super('Sale', injector);
-   }
-
-   getAllSales(): Observable<any> {
-    return this.http.post<any>(
-      this.claims.webapi_url + '/Agents/Archive/GetArchiveListByColumns2',
-      this.queryAllSales, this.options).pipe(catchError(this.onError));
+    super("Sale", injector);
   }
 
-   /** Gets or creates a sale depended on saleId. 0 will create a new sale */
-   getSale(saleId): Observable<any> {
+  getAllSales(): Observable<any> {
+    return this.http
+      .get<any>(
+        `${
+          this.claims.webapi_url
+        }/sale?$select=saleId,heading,description&$top=20`,
+        this.options
+      )
+      .pipe(catchError(this.onError));
+  }
+
+  /** Gets or creates a sale depended on saleId. 0 will create a new sale */
+  getSale(saleId): Observable<any> {
     return this.get(saleId);
   }
 
